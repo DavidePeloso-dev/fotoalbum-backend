d2e3dd6b218783e903dc7514f5ae55fcc460c127
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-06-09 13:57:12 +0200

    Initial commit CRUD Photo

---
+++ database/seeders/PhotoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Photo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PhotoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $photo = new Photo();
+            $photo->title = $faker->words(3, true);
+            $photo->description = $faker->paragraphs(4, true);
+            $photo->photo = $faker->imageUrl(640, 480, 'Photos', true, $photo->title, true, 'jpg');
+            $photo->save();
+        }
+    }
+}
